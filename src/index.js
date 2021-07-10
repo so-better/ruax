694dc3f5723a228e5bb9bbbb10b6c34f38911a4a
lovelin0523 <18356201582@163.com>
2021-07-10 15:13:13 +0800

    更新

--- src/index.js
+++ src/index.js
@@ -8,94 +8,96 @@ class Ruax {
 	//对create的参数config进行初始化，获取初始化后的config
 	_getValidationConfig(config) {
 		//config非对象,默认为defaults
-		if (typeof(config) != "object") {
+		if (typeof config != "object") {
 			config = {};
 			Object.assign(config, this.defaults);
 		}
 		//baseUrl基本路径
-		if (typeof(config.baseUrl) != 'string') {
+		if (typeof config.baseUrl != 'string') {
 			config.baseUrl = this.defaults.baseUrl;
 		}
 		//url路径
-		if (typeof(config.url) != 'string') {
+		if (typeof config.url != 'string') {
 			config.url = this.defaults.url;
 		}
 		//data请求数据
-		if (typeof(config.data) != "object") {
+		if (typeof config.data != "object") {
 			config.data = this.defaults.data;
 		} else { //如果配置的config有data则将defaults的追加进来	
-			var obj = config.data;
+			let obj = config.data;
 			config.data = {};
 			Object.assign(config.data, this.defaults.data);
 			Object.assign(config.data, obj);
 		}
 		//type请求方式post/get
-		if ((typeof(config.type) != "string") || ((config.type.toUpperCase() != 'POST') && (config.type.toUpperCase() !=
+		if ((typeof config.type != "string") || ((config.type.toLocaleUpperCase() != 'POST') && (config.type
+				.toLocaleUpperCase() !=
 				'GET'))) {
 			config.type = this.defaults.type;
 		}
 		//timeout请求超时时间
-		if (typeof(config.timeout) != "number" || isNaN(config.timeout)) {
+		if (typeof config.timeout != "number" || isNaN(config.timeout)) {
 			config.timeout = this.defaults.timeout;
 		}
 		//dataType返回参数类型
-		if ((typeof(config.dataType) != "string") || ((config.dataType.toUpperCase() != "STRING") &&
-				(config.dataType.toUpperCase() != "XML") && (config.dataType.toUpperCase() != "HTML") &&
-				(config.dataType.toUpperCase() != "JSON") && (config.dataType.toUpperCase() != "BLOB") &&
-				(config.dataType.toUpperCase() != "JSONP"))) {
+		if ((typeof config.dataType != "string") || ((config.dataType.toLocaleUpperCase() != "STRING") &&
+				(config.dataType.toLocaleUpperCase() != "XML") && (config.dataType.toLocaleUpperCase() != "HTML") &&
+				(config.dataType.toLocaleUpperCase() != "JSON") && (config.dataType.toLocaleUpperCase() !=
+				"BLOB") &&
+				(config.dataType.toLocaleUpperCase() != "JSONP"))) {
 			config.dataType = this.defaults.dataType;
 		}
 		//jsonpCallback跨域回调方法名称
-		if (typeof(config.jsonpCallback) != 'string') {
+		if (typeof config.jsonpCallback != 'string') {
 			config.jsonpCallback = this.defaults.jsonpCallback;
 		}
 		//headers请求头配置
-		if (typeof(config.headers) != "object") {
+		if (typeof config.headers != "object") {
 			config.headers = this.defaults.headers;
 		} else { //如果配置的config有headers则将defaults的追加进来
-			var obj = config.headers;
+			let obj = config.headers;
 			config.headers = {};
 			Object.assign(config.headers, this.defaults.headers);
 			Object.assign(config.headers, obj);
 		}
 		//contentType配置
-		if (typeof(config.contentType) != 'string' && config.contentType !== false){
+		if (typeof config.contentType != 'string' && config.contentType !== false) {
 			config.contentType = this.defaults.contentType;
 		}
 		//processData配置
-		if (typeof(config.processData) != 'boolean'){
+		if (typeof config.processData != 'boolean') {
 			config.processData = this.defaults.processData;
 		}
 		//cache缓存配置
-		if (typeof(config.cache) != 'boolean') {
+		if (typeof config.cache != 'boolean') {
 			config.cache = this.defaults.cache;
 		}
 		//async异步
-		if (typeof(config.async) != 'boolean') {
+		if (typeof config.async != 'boolean') {
 			config.async = this.defaults.async;
 		}
 		//请求发送前
-		if (typeof(config.beforeSend) != 'function') {
+		if (typeof config.beforeSend != 'function') {
 			config.beforeSend = this.defaults.beforeSend;
 		}
 		//请求完成
-		if (typeof(config.complete) != 'function') {
+		if (typeof config.complete != 'function') {
 			config.complete = this.defaults.complete;
 		}
 		//请求进度
-		if (typeof(config.onProgress) != 'function') {
+		if (typeof config.onProgress != 'function') {
 			config.onProgress = this.defaults.onProgress;
 		}
 		//取消请求
-		if (typeof(config.cancelRequest) != "function") {
+		if (typeof config.cancelRequest != "function") {
 			config.cancelRequest = this.defaults.cancelRequest;
 		}
 		//请求发送之前对数据进行处理
-		if (typeof(config.beforeRequest) != "function") {
+		if (typeof config.beforeRequest != "function") {
 			config.beforeRequest = this.defaults.beforeRequest;
 		}
 		//请求响应前对响应结果进行处理
-		if (typeof(config.beforeResponse) != "function") {
+		if (typeof config.beforeResponse != "function") {
 			config.beforeResponse = this.defaults.beforeResponse;
 		}
 		return config;
@@ -104,91 +106,95 @@ class Ruax {
 	//设置全局默认数据
 	_setDefaults() {
 		//defaults非对象,默认为空对象
-		if (typeof(this.defaults) != "object") {
+		if (typeof this.defaults != "object") {
 			this.defaults = {};
 		}
 		//baseUrl基本路径
-		if (typeof(this.defaults.baseUrl) != 'string') {
+		if (typeof this.defaults.baseUrl != 'string') {
 			this.defaults.baseUrl = '';
 		}
 		//url路径
-		if (typeof(this.defaults.url) != 'string') {
+		if (typeof this.defaults.url != 'string') {
 			this.defaults.url = '';
 		}
 		//data请求数据
-		if (typeof(this.defaults.data) != "object") {
+		if (typeof this.defaults.data != "object") {
 			this.defaults.data = {};
 		}
 		//type请求方式post/get
-		if ((typeof(this.defaults.type) != "string") || ((this.defaults.type.toUpperCase() != 'POST') && (this.defaults.type
-				.toUpperCase() != 'GET'))) {
+		if ((typeof this.defaults.type != "string") || ((this.defaults.type.toLocaleUpperCase() != 'POST') && (this
+				.defaults.type
+				.toLocaleUpperCase() != 'GET'))) {
 			this.defaults.type = 'GET';
 		}
 		//timeout请求超时时间
-		if (typeof(this.defaults.timeout) != "number" || isNaN(this.defaults.timeout)) {
+		if (typeof this.defaults.timeout != "number" || isNaN(this.defaults.timeout)) {
 			this.defaults.timeout = 8000;
 		}
 		//dataType返回参数类型
-		if ((typeof(this.defaults.dataType) != "string") || ((this.defaults.dataType.toUpperCase() != "STRING") &&
-				(this.defaults.dataType.toUpperCase() != "XML") && (this.defaults.dataType.toUpperCase() != "HTML") &&
-				(this.defaults.dataType.toUpperCase() != "JSON") && (this.defaults.dataType.toUpperCase() != "BLOB") &&
-				(this.defaults.dataType.toUpperCase() != "JSONP"))) {
+		if ((typeof this.defaults.dataType != "string") || ((this.defaults.dataType.toLocaleUpperCase() !=
+				"STRING") &&
+				(this.defaults.dataType.toLocaleUpperCase() != "XML") && (this.defaults.dataType
+				.toLocaleUpperCase() != "HTML") &&
+				(this.defaults.dataType.toLocaleUpperCase() != "JSON") && (this.defaults.dataType
+				.toLocaleUpperCase() != "BLOB") &&
+				(this.defaults.dataType.toLocaleUpperCase() != "JSONP"))) {
 			this.defaults.dataType = 'JSON';
 		}
 		//jsonpCallback跨域回调方法名称
-		if (typeof(this.defaults.jsonpCallback) != 'string') {
+		if (typeof this.defaults.jsonpCallback != 'string') {
 			this.defaults.jsonpCallback = 'callback';
 		}
 		//headers请求头配置
-		if (typeof(this.defaults.headers) != "object") {
+		if (typeof this.defaults.headers != "object") {
 			this.defaults.headers = {}
 		}
 		//ContentType配置
-		if (typeof(this.defaults.contentType) != 'string' && this.defaults.contentType !== false){
+		if (typeof this.defaults.contentType != 'string' && this.defaults.contentType !== false) {
 			this.defaults.contentType = 'application/x-www-form-urlencoded';
 		}
 		//processData配置
-		if (typeof(this.defaults.processData) != 'boolean'){
+		if (typeof this.defaults.processData != 'boolean') {
 			this.defaults.processData = true;
 		}
 		//cache缓存配置
-		if (typeof(this.defaults.cache) != 'boolean') {
+		if (typeof this.defaults.cache != 'boolean') {
 			this.defaults.cache = true;
 		}
 		//async异步
-		if (typeof(this.defaults.async) != 'boolean') {
+		if (typeof this.defaults.async != 'boolean') {
 			this.defaults.async = true;
 		}
 		//请求发送前
-		if (typeof(this.defaults.beforeSend) != 'function') {
+		if (typeof this.defaults.beforeSend != 'function') {
 			this.defaults.beforeSend = function() {};
 		}
 		//请求完成
-		if (typeof(this.defaults.complete) != 'function') {
+		if (typeof this.defaults.complete != 'function') {
 			this.defaults.complete = function() {};
 		}
 		//请求进度
-		if (typeof(this.defaults.onProgress) != 'function') {
+		if (typeof this.defaults.onProgress != 'function') {
 			this.defaults.onProgress = function() {};
 		}
 		//取消请求
-		if (typeof(this.defaults.cancelRequest) != "function") {
+		if (typeof this.defaults.cancelRequest != "function") {
 			this.defaults.cancelRequest = function() {};
 		}
 		//请求发送前对数据处理的方法
-		if (typeof(this.defaults.beforeRequest) != "function") {
+		if (typeof this.defaults.beforeRequest != "function") {
 			this.defaults.beforeRequest = function() {};
 		}
 		//请求发送相应前处理结果的方法
-		if (typeof(this.defaults.beforeResponse) != "function") {
+		if (typeof this.defaults.beforeResponse != "function") {
 			this.defaults.beforeResponse = function() {};
 		}
 	}
 
 	//将json类型的数据转为&拼接的字符串
 	_getParams(data) {
-		var arr = [];
-		for (var param in data) {
+		let arr = [];
+		for (let param in data) {
 			arr.push(encodeURIComponent(param) + '=' + encodeURIComponent(data[param]));
 		}
 		return arr.join('&');
@@ -199,10 +205,10 @@ class Ruax {
 		//初始化defaults数据
 		this._setDefaults();
 		//初始化beforeRequest和beforeResponse
-		if (typeof(this.beforeRequest) != 'function') {
+		if (typeof this.beforeRequest != 'function') {
 			this.beforeRequest = function() {};
 		}
-		if (typeof(this.beforeResponse) != 'function') {
+		if (typeof this.beforeResponse != 'function') {
 			this.beforeResponse = function() {};
 		}
 		//校验config数据
@@ -213,9 +219,9 @@ class Ruax {
 		//Promise回调
 		return new Promise((resolve, reject) => {
 			//创建XMLHttpRequest对象
-			var xhr = new XMLHttpRequest();
+			let xhr = new XMLHttpRequest();
 			//如果是媒体文件则设置responseType="blob"
-			if (config.dataType.toLowerCase() == "blob") {
+			if (config.dataType.toLocaleLowerCase() == "blob") {
 				xhr.responseType = "blob";
 			}
 			//给请求添加状态变化事件处理函数
@@ -223,27 +229,28 @@ class Ruax {
 				if (xhr.readyState == 4) {
 					config.complete(xhr);
 					if (xhr.status == 200) {
-						var res;
-						if (config.dataType.toLowerCase() == "json") {
+						let res;
+						if (config.dataType.toLocaleLowerCase() == "json") {
 							try {
 								res = JSON.parse(xhr.responseText);
 							} catch (error) {
 								//json解析失败
 								reject(error);
 							}
-						} else if (config.dataType.toLowerCase() == "xml") {
+						} else if (config.dataType.toLocaleLowerCase() == "xml") {
 							res = xhr.responseXML;
-						} else if (config.dataType.toLowerCase() == "html" || config.dataType == "string") {
+						} else if (config.dataType.toLocaleLowerCase() == "html" || config.dataType ==
+							"string") {
 							res = xhr.responseText;
-						} else if (config.dataType.toLowerCase() == "blob") {
+						} else if (config.dataType.toLocaleLowerCase() == "blob") {
 							res = xhr.response;
 						} else {
 							res = xhr.responseText;
 						}
-						this.beforeResponse(xhr,res);
-						config.beforeResponse(xhr,res);
+						this.beforeResponse(xhr, res);
+						config.beforeResponse(xhr, res);
 						resolve(res);
-					} else if(xhr.status != 0){
+					} else if (xhr.status != 0) {
 						this.beforeResponse(xhr);
 						config.beforeResponse(xhr);
 						reject(new Error('Request failed with status code ' + xhr.status));
@@ -257,7 +264,7 @@ class Ruax {
 			xhr.ontimeout = e => {
 				this.beforeResponse(xhr);
 				config.beforeResponse(xhr);
-				reject(new Error('timeout of '+ config.timeout +'ms exceeded'));
+				reject(new Error('timeout of ' + config.timeout + 'ms exceeded'));
 			}
 
 			//监听上传进度
@@ -265,13 +272,13 @@ class Ruax {
 				config.onProgress(e);
 			}
 
-			if (config.dataType.toLowerCase() == "jsonp") {
+			if (config.dataType.toLocaleLowerCase() == "jsonp") {
 				config.beforeSend();
 				//创建 script 标签并加入到页面中
-				var callbackName = ('jsonp_' + Math.random()).replace(".", "");
-				var oHead = document.getElementsByTagName('head')[0];
+				let callbackName = ('jsonp_' + Math.random()).replace(".", "");
+				let oHead = document.getElementsByTagName('head')[0];
 				config.data[config.jsonpCallback] = callbackName;
-				var oS = document.createElement('script');
+				let oS = document.createElement('script');
 				oHead.appendChild(oS);
 				//创建jsonp回调函数
 				window[callbackName] = (result) => {
@@ -296,12 +303,13 @@ class Ruax {
 					oHead.removeChild(oS);
 					this.beforeResponse();
 					config.beforeResponse();
-					reject(new Error('timeout of '+ config.timeout +'ms exceeded'));
+					reject(new Error('timeout of ' + config.timeout + 'ms exceeded'));
 				}, config.timeout);
 			} else {
-				if (config.type.toLowerCase() == 'get') {
+				if (config.type.toLocaleLowerCase() == 'get') {
 					if (this._getParams(config.data)) {
-						xhr.open("GET", config.baseUrl + config.url + '?' + this._getParams(config.data), config
+						xhr.open("GET", config.baseUrl + config.url + '?' + this._getParams(config.data),
+							config
 							.async);
 					} else {
 						xhr.open("GET", config.baseUrl + config.url, config.async);
@@ -311,29 +319,29 @@ class Ruax {
 					}
 
 					//添加配置的请求头
-					for (var item in config.headers) {
+					for (let item in config.headers) {
 						xhr.setRequestHeader(item, config.headers[item]);
 					}
 					//添加ContentType
-					if(typeof(config.contentType) == 'string'){
-						xhr.setRequestHeader('Content-Type',config.contentType)
+					if (typeof config.contentType == 'string') {
+						xhr.setRequestHeader('Content-Type', config.contentType)
 					}
 					xhr.send(null)
-				} else if (config.type.toLowerCase() == 'post') {
+				} else if (config.type.toLocaleLowerCase() == 'post') {
 					xhr.open('POST', config.baseUrl + config.url, config.async);
 					if (config.async == true) { //异步才设置超时时间
 						xhr.timeout = config.timeout; //设置超时时间
 					}
 
 					//添加配置的请求头
-					for (var item in config.headers) {
+					for (let item in config.headers) {
 						xhr.setRequestHeader(item, config.headers[item]);
 					}
 					//添加ContentType
-					if(typeof(config.contentType) == 'string'){
-						xhr.setRequestHeader('Content-Type',config.contentType)
+					if (typeof config.contentType == 'string') {
+						xhr.setRequestHeader('Content-Type', config.contentType)
 					}
-					if(config.processData){
+					if (config.processData) {
 						config.data = this._getParams(config.data); //转换成序列化参数
 					}
 					xhr.send(config.data);
@@ -368,6 +376,7 @@ class Ruax {
 
 const packages = require('../package.json');
 
-console.log('%c感谢使用'+packages.name+'，当前版本：%c v'+packages.version+'\n%c'+packages.name+'完全由个人开发，如果你觉得还不错的话，欢迎前往github给个star，感谢！\ngithub地址：%c'+packages.github,'color:#808080;','color:#008a00','color:#808080;','color:#008a00');
-
+console.log('%c感谢使用' + packages.name + '，当前版本：%c v' + packages.version + '\n%c如果你觉得' + packages.name +
+	'还不错，不妨去github点个star\ngithub地址：%c' + packages.github, 'color:#808080;', 'color:#008a00',
+	'color:#808080;', 'color:#008a00');
 module.exports = Ruax
